39de03e156cbdfbca82f2e69083ecded2351b0ff
Dan Rakotoarison <danielorak1@gmail.com>
2021-08-11 14:14:59 +0100

    add category entity configure front and make fixture

--- src/Controller/front/HomeController.php
+++ src/Controller/front/HomeController.php
@@ -3,6 +3,7 @@ namespace App\Controller\front;
 
 use App\Services\ArticleService;
 use App\Repository\ArticlesRepository;
+use App\Services\CategoryService;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,10 +11,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     private $articleService;
+    private $categorie;
 
-    public function __construct(ArticleService $_articleService)
+    public function __construct(ArticleService $_articleService, CategoryService $_categorie)
     {
         $this->articleService = $_articleService;
+        $this->categorie = $_categorie;
     }
 
 
@@ -24,11 +27,14 @@ class HomeController extends AbstractController
     {
         $max = false;
         $articles = $this->articleService->getAllArticles($max);
+        $categories = $this->categorie->getAllCategories();
+        
         $articlesInSesion = $_session->get('article');
         // dd($articlesInSesion);
 
         return $this->render('/front/base-fo.html.twig', [
-                                                         'articles' => $articles,
+                                                         'articles'   => $articles,
+                                                         'categories' => $categories,
                                                          ]);
     }
 
