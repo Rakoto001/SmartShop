20d045ffcda856f2cdb14ba6ed1bb231815247b3
Dan Rakotoarison <danielorak1@gmail.com>
2021-08-15 13:04:04 +0100

    refont user registration

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Services\UserService;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
@@ -15,7 +17,11 @@ class UserType extends AbstractType
             ->add('username')
             ->add('email')
             ->add('status')
-            ->add('gender')
+            ->add('gender', ChoiceType::class, [
+                                                'label'   => false,
+                                                'choices' => array_flip(UserService::GENDER),
+                                                'mapped'  => false,
+                                                 ])
             ->add('lastname')
             // ->add('password')
             ->add('avatar')
