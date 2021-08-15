87879ac195139669a62cc072106def592947a3e2
Dan Rakotoarison <danielorak1@gmail.com>
2021-08-15 11:57:31 +0100

    add a user registration form

--- src/Controller/back/user/UserController.php
+++ src/Controller/back/user/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\back\user;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -16,5 +19,24 @@ class UserController extends AbstractController
     public function list()
     {
         return $this->render('back/user/user-list.html.twig');
+    }
+
+    /**
+     * @Route("/create", name="admin_user_create")
+     */
+    public function userCreate(Request $request)
+    {
+        $user = new User;
+        $userForm = $this->createForm(UserType::class, $user);
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+                dd($userForm);
+        }
+
+        return $this->render('back/user/user-action.html.twig', [
+                                                                    'userForm' => $userForm->createView(),
+        ]);
+
     }
 }
