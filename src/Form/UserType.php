3758b16b94e1eea679dc2aae270e5a7fea827008
Dan Rakotoarison <danielorak1@gmail.com>
2021-08-16 13:10:02 +0100

    initiaise miltiple select roles on UserType

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,6 +7,7 @@ use App\Services\UserService;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
@@ -15,17 +16,27 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username')
-            ->add('email')
-            ->add('status')
+            ->add('email', EmailType::class)
+            ->add('status', ChoiceType::class, [
+                                                // 'label'   => false,
+                                                'choices' => array_flip(UserService::STATUS),
+                                                'choices' => array_flip(UserService::STATUS),
+                                                // 'mapped'  => false,
+            ])
             ->add('gender', ChoiceType::class, [
-                                                'label'   => false,
+                                                // 'label'   => false,
                                                 'choices' => array_flip(UserService::GENDER),
-                                                'mapped'  => false,
+                                                // 'mapped'  => false,
                                                  ])
-            ->add('lastname')
-            // ->add('password')
-            ->add('avatar')
-        ;
+            ->add('role', ChoiceTYpe::class, [
+                                                'choices' => array_flip([
+                                                                            'ROLE_USER'  => 'UTILISATEUR',
+                                                                            'ROLE_ADMIN' => 'ADMINISTRATEUR'
+                                                            ]),
+                                                'multiple' => true,
+                                                'mapped'  => false,
+                                             ])
+            ->add('lastname');
     }
 
     public function configureOptions(OptionsResolver $resolver)
