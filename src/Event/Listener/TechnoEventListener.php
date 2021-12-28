d193ef8cd52b9572afc459d704bcdc348831c9ff
Danielo RAKOTOARISON <d.rakotoarison@etech.local>
2021-12-28 16:40:47 +0300

    delete session  with event and add flash notifications

--- src/Event/Listener/TechnoEventListener.php
+++ src/Event/Listener/TechnoEventListener.php
@@ -36,25 +36,9 @@ class TechnoEventListener
         $totalPrice     = $paramsArticles['totalPrice'];
         $odate          = new DateTime();
         $purshaseDate   = $odate->format('Y-m-d H:i:s');
-
-        // return $this->render('mail/adminmail/admin.html.twig', [
-        //                                                         'cartArticles'  => $cartArticles,
-        //                                                         'total'         => $totalPrice,
-        //                                                         'purshaseDate'  => $purshaseDate
-
         $this->mailerService->sendMailToAdmin($paramsArticles, $purshaseDate);
-    //     $this->mailerService->sendMailConfirmToUser();
-    //     // $message = (new \Swift_Message('Hello Email'))
-    //     $message = \Swift_Message::newInstance()
-    //     ->setFrom('symfony9494@gmail.com')
-    //     ->setTo('rakotoarisondan@gmail.com')
-    //     ->setBody("Envoi 12 Mail by Dan", 'text/html')
-    // ;
-return true;
 
-
-    // $this->mailer->send($message);
-    // $output->writeln('Successful you send a self email');
+        return true;
     }
 
 }
